7fb8713e66cedaa7177265ed8cd7da17a44b8a42
arvid-e <arvid.edvinsson@gmail.com>
2026-08-19 16:01:17 +0900

    feat: add transactions schema migrations

---
+++ src/main/resources/db/migration/V3__create_transactions_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE transactions (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id UUID NOT NULL REFERENCES users(id),
+  type VARCHAR(20) NOT NULL,
+  status VARCHAR(20) NOT NULL,
+  idempotency_key UUID NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  UNIQUE (user_id, idempotency_key),
+  CONSTRAINT transaction_type_valid
+    CHECK (type IN ('DEPOSIT', 'WITHDRAW', 'TRANSFER')),
+  CONSTRAINT transaction_status_valid
+    CHECK (status IN ('PENDING', 'COMPLETED', 'FAILED'))
+)
